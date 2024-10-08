320a8adf3912b3f1d7d00c2c9c0ccdc1b0781b96
slitt01 <slitt01@students.cs.ubc.ca>
2024-10-08 16:17:04 -0700

    Added tests for FoodItem class

---
+++ src/test/FoodItemTest.java
@@ -0,0 +1,52 @@
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import model.FoodItem;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FoodItemTest {
+
+    private FoodItem foodItem;
+
+    @BeforeEach
+    void runBefore() {
+        foodItem = new FoodItem("Banana", 100, 1.5);
+
+    }
+
+    @Test
+    void testConstructor() {
+        assertEquals("Banana", foodItem.getFoodName());
+        assertEquals(100, foodItem.getCaloriesPerPortion());
+        assertEquals(1.5, foodItem.getPortionSize());
+
+    }
+
+    @Test
+    void testSetFoodName() {
+        foodItem.setFoodName("Mango");
+        assertEquals("Mango", foodItem.getFoodName());
+
+    }
+    @Test
+    void testSetCaloriesPerPortion() {
+        foodItem.setCaloriesPerPortion(200);
+        assertEquals(200, foodItem.getCaloriesPerPortion());
+    }
+
+    @Test 
+    void testSetPortionSize() {
+        foodItem.setPortionSize(2);
+        assertEquals(2, foodItem.getPortionSize());
+    }
+    
+    @Test
+    void testCalculateTotalCalories() {
+        assertEquals(150, foodItem.calculateTotalCalories());
+    }
+
+
+}
